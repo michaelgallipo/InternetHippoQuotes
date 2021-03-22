ea254499bc2b17b4017177d9d7cb4b22aff6b12e
michael <michael.gallipo@gmail.com>
2021-03-22 00:31:52 -0400

    added .env, other packages, continued wiring backend

--- routes/Index.js
+++ routes/Index.js
@@ -1,8 +1,31 @@
 const express = require("express");
+const { check, validationResult } = require("express-validator");
 
 const router = express.Router();
 router.get("/", (req, res) => {
 	res.render("form");
 });
+
+router.post(
+	"/",
+	[
+		check("quote").isLength({ min: 1 }).withMessage("Please enter a quote"),
+		// check('email')
+		//   .isLength({ min: 1 })
+		//   .withMessage('Please enter an email'),
+	],
+	(req, res) => {
+		const errors = validationResult(req);
+		if (errors.isEmpty()) {
+			res.send("Quote Successfully Stored");
+		} else {
+			res.render("form", {
+				title: "Quote Submission",
+				errors: errors.array(),
+				data: req.body,
+			});
+		}
+	}
+);
 
 module.exports = router;
--- start.js
+++ start.js
@@ -1,4 +1,6 @@
 const app = require("./app");
+require("dotenv").config();
+const mongoose = require("mongoose");
 
 const server = app.listen(3000, () => {
 	console.log(`Express is running on port ${server.address().port}`);
